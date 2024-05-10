f2414b80d59c6dc6e9bea1d8ac6ccd65b2146347
novilla7 <nopilla7@gmail.com>
2024-05-10 21:14:18 +0700

    Interacting

---
+++ cypress/e2e/0-personal-learn/first.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+//contoh eduwork
+// describe('My First Test', () => {
+//     it('Visits the Kitchen Sink', () => {
+//         cy.visit('https://example.cypress.io')
+//         cy.get('h1').contains('Kitchen Sink')
+//         cy.contains('get').click()
+//         cy.url().should('include', '/commands/querying')
+//     });
+// });
+
+
+describe('My First Test', () => {
+    it('clicking "type" show the right headings', () => {
+        cy.visit('https://example.cypress.io');
+
+        // cy.pause(); // tidak dijalankan karena jika dijalankan akan berhenti dan tidak menjalankan script kebawah atau lanjutannya
+        
+        cy.contains('type').click();
+
+        // Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions');
+
+        // Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
+    });
+});
---
+++ cypress/e2e/0-personal-learn/interacting.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+
+//contoh eduwork
+// describe('Browser actions', () => {
+
+//     it('Should load books website', () => {
+//         cy.visit('https://books.toscrape.com/index.html', { timeout: 10000 })
+//         cy.url().should('include', 'index.html')
+// });
+
+//     it('Should click on Travel Category', () => {
+//         cy.get('a').contains('Travel').click()
+//         cy.get('h1').should('have.text', 'Travel')
+
+//  });
+// });
+
+
+describe('Working with inputs', () => {
+
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+});
+
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username').should('have.value', 'username')
+});
+
+    it('Should fill password', () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type('password').should('have.value', 'password')
+});
+
+    it('Should checklist', () => {
+        cy.get('#user_remember_me').check()
+        cy.get('#user_remember_me').should('be.checked')
+})
+   
+});
